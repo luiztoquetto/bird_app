66edd5a18855ea8cee6cc59198d6079bbec22b20
luiztoquetto <luftoquetto@gmail.com>
2021-06-01 14:06:52 -0300

    feat(app): added initial files for app

---
+++ app/src/app/pages/main/main.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bird-main',
+  templateUrl: './main.page.html',
+  styleUrls: ['./main.page.scss'],
+})
+export class MainPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
